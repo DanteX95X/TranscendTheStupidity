e543aeb46f4713b374ee1b8c516ac9c1f247182d
dante <daniellewinski95@gmail.com>
2017-07-01 16:57:50 +0200

    Lemmings are facing direction they are walking at.

--- Assets/Scripts/Game/Lemming.cs
+++ Assets/Scripts/Game/Lemming.cs
@@ -38,17 +38,22 @@ namespace Assets.Scripts.Game
 		{
 			Vector3 approximatePosition = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), transform.position.z);
 			if (currentField == null || (currentField.transform.position != currentLevel.Fields[approximatePosition].transform.position && (transform.position - currentLevel.Fields[approximatePosition].transform.position).magnitude < 0.1))
+			{
+				currentField = currentLevel.Fields[approximatePosition];
+				List<Field> path = PathFinding.AStar(currentField, currentLevel.Fields[goal], PathFinding.EuclideanHeuristic);
+				if (path.Count > 1)
 				{
-					currentField = currentLevel.Fields[approximatePosition];
-					List<Field> path = PathFinding.AStar(currentField, currentLevel.Fields[goal], PathFinding.EuclideanHeuristic);
-					if (path.Count > 1)
-						{
-							velocity = (path[1].transform.position - transform.position).normalized;
-						} else
-						{
-							Destroy(gameObject);
-						}
+					velocity = (path[1].transform.position - transform.position).normalized;
+					if(velocity.x > 0)
+						GetComponent<SpriteRenderer>().flipX = false;
+					else if(velocity.x < 0)
+						GetComponent<SpriteRenderer>().flipX = true;
+				} 
+				else
+				{
+					Destroy(gameObject);
 				}
+			}
 
 			transform.position += velocity * speed * Time.deltaTime;
 
