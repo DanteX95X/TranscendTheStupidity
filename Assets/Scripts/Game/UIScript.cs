80c146e1b84e877495e5d8b97fb0e9307105cc8e
dante <daniellewinski95@gmail.com>
2017-07-02 01:47:50 +0200

    Group UI can now be hidden.

---
+++ Assets/Scripts/Game/UIScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Game
+{
+	public class UIScript : MonoBehaviour 
+	{
+
+		[SerializeField]
+		GameObject[] ui = new GameObject[6];
+
+		void Start () 
+		{
+		
+		}
+
+		void Update () 
+		{
+		
+		}
+
+		public void Trigger(int group)
+		{
+			foreach(GameObject temp in ui)
+				temp.SetActive(false);
+			ui[group].SetActive(true);
+		}
+	}
+}
